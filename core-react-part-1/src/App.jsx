5f46d4f171c707d6457e43b9defb7da31fedc644
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-05-19 22:56:00 +0600

    component

--- core-react-part-1/src/App.jsx
+++ core-react-part-1/src/App.jsx
@@ -6,21 +6,52 @@ function App() {
   return (
     <>
       
-      <h1>Vite + React</h1>
+      <h1>React Core Concept</h1>
+      <Student></Student>
+      <Student></Student>
       <Person></Person>
-      <Sports></Sports>
-      <Person></Person>
-      <Person></Person>
-      <Sports></Sports>
+      <Developer name="Zim" tech="Python"></Developer>
+      <Developer name="Sahel" tech="JavaScript"></Developer>
+      <Developer name="Jobaer" tech="Java"></Developer>
+      <Device dname="acer" user="Jobaer" year="2"></Device>
+      <Device dname="Walton" user="Shakil" year="unknown"></Device>
+      <Device dname="hp" user="Abir" year="unknown"></Device>
       
     </>
   )
 }
 
+function Developer(props){
+  console.log(props);
+  return(
+    <div style={{
+      border: '2px solid green',
+      borderRadius: '20px'
+    }}>
+      <h3>Developer:{props.name}</h3>
+      <p>Technology:{props.tech}</p>
+    </div>
+  )
+}
+
+function Device(devinfo){
+  return(
+    <div>
+      <h2>Device Name:{devinfo.dname}</h2>
+      <h1>User:{devinfo.user}</h1>
+      <p>Years:{devinfo.year}</p>
+    </div>
+  )
+}
+
 function Person(){
   const age = 24;
+  const personStyle = {
+    color: 'red',
+    textAlign: 'right'
+  }
   return (
-    <p>I am a person {age}</p>
+    <p style={personStyle}>I am a person {age}</p>
   )
 }
 
@@ -29,6 +60,15 @@ function Sports(){
     <div>
       <h1>Cricekt</h1>
       <p>Playing and loosing</p>
+    </div>
+  )
+}
+
+function Student(){
+  return(
+    <div className='student'>
+      <p>Name:</p>
+      <p>Department:</p>
     </div>
   )
 }
