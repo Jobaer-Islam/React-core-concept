320958b93cfeef0816b84a15a7af1f571d948d42
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-05-21 19:48:37 +0600

    added styling and conditional rendering , toggle

--- react-on-the-go/src/components/countries/Countries.jsx
+++ react-on-the-go/src/components/countries/Countries.jsx
@@ -1,18 +1,20 @@
 import React, { use } from 'react';
 import Country from '../country/Country';
-
-const Countries = ({countriesPromise}) => {
+import './countries.css'
+const Countries = ({ countriesPromise }) => {
 
     const countries = use(countriesPromise);
     console.log(countries);
-    
+
     return (
         <div>
             <h1>Traveling Countries: {countries.length}</h1>
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-                
-            }
+            <div className='countries'>
+                {
+                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+
+                }
+            </div>
         </div>
     );
 };
--- react-on-the-go/src/components/country/Country.jsx
+++ react-on-the-go/src/components/country/Country.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './country.css'
 const Country = ({country}) => {
-    console.log(country);
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        // if(visited === true){
+        //     setVisited(false);
+        // }
+        // else{
+        //     setVisited(true);
+        // }
+        setVisited(!visited);
+        
+    }
    
     return (
-        <div>
+        <div className={`country ${visited && 'country-visited'} `}>
+
             <h3>Name: {country.name.common}</h3>
             <img src={country.flags.png} alt="" />
             <p>Indepent: {country.independent ? "Free": "Not Free"}</p>
             <p>Population: {country.population} </p>
+            <button onClick={handleVisited}> {
+                visited ? 'Visited' : 'Not Visited'
+                }</button>
+
         </div>
     );
 };
 
-export default Country;
+export default Country;  
