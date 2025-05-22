e63d3c9d1fb5ac0ac37a98e14c1cc71073871638
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-05-22 20:18:50 +0600

    added cart and remove functionality

--- awesome-water-bottles/src/components/Bottles/Bottles.jsx
+++ awesome-water-bottles/src/components/Bottles/Bottles.jsx
@@ -1,6 +1,8 @@
-import React, { use, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import Bottle from '../Bottle/Bottle';
 import './Bottles.css'
+import { addItemToCartLocalStorage, getCartFromLocalStorage } from '../../utilities/localStorage';
+import Cart from '../Cart/Cart';
 
 const Bottles = ({ bottlesPromise }) => {
 
@@ -8,8 +10,42 @@ const Bottles = ({ bottlesPromise }) => {
 
     const bottles = use(bottlesPromise);
 
+    // useEffect 
+    useEffect( ()=>{
+         const storedCardIds = getCartFromLocalStorage();
+        //  console.log(storedCardIds , bottles);
+
+        const storedCart = [];
+
+        for(const id of storedCardIds){
+            // console.log(id);
+            const cartBottle = bottles.find(bottle => bottle.id === id);
+
+            if(cartBottle){
+                storedCart.push(cartBottle);
+            }
+        }
+
+        console.log('stored cart', storedCart);
+        setCart(storedCart);
+        
+         
+    }, [bottles])
+
     const handleAddToCart = (bottle) => {
-        console.log("bottle added", bottle);
+        // console.log("bottle added", bottle);
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+
+        // save the bottle id in the Storage
+        addItemToCartLocalStorage(bottle.id);
+        
+    }
+
+    const handleRemoveFromCart = id => {
+        console.log('remove item from the cart');
+        const remainingCart = cart.filter(bottle => bottle.id !== id);
+        setCart(remainingCart);
         
     }
 
@@ -18,6 +54,8 @@ const Bottles = ({ bottlesPromise }) => {
     return (
         <div>
             <h3>Bottles: {bottles.length}</h3>
+            <p>Added to Cart: {cart.length}</p>
+            <Cart cart = {cart} handleRemoveFromCart = {handleRemoveFromCart}> </Cart>
             <div className='bottles-container'>
                 {
                     bottles.map(bottle => <Bottle
