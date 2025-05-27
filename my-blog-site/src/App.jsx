36cce4a7be5f79fb7c48a88898177a1ee4c247ea
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-05-27 18:17:44 +0600

    added card functionality

--- my-blog-site/src/App.jsx
+++ my-blog-site/src/App.jsx
@@ -1,13 +1,19 @@
 
+import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
+  const [bookmarked, setBookmarked] = useState([]);
+
   const handleBookMark = (blog) =>{
-    console.log(blog);
+    // console.log(blog);
+    setBookmarked([...bookmarked, blog]);
   }
+
+  // console.log(bookmarked);
 
   return (
     <>
@@ -22,6 +28,9 @@ function App() {
         <div className="right-container w-[30%]">
           <h1>Reading time: 0 </h1>
           <h1>Bookmarked count: 0</h1>
+          {
+            bookmarked.map((marked)=><p>{marked.title}</p>)
+          }
         </div>
       </div>
     </>
