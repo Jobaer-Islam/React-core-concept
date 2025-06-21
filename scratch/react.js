5add04e0253bfe95cdf4f4dd17f12b1667b7b4aa
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-06-21 21:51:00 +0600

    props

--- scratch/react.js
+++ scratch/react.js
@@ -1,5 +1,5 @@
-console.log(React);
-console.log(ReactDOM);
+// console.log(React);
+// console.log(ReactDOM);
 
 const myReactElement = React.createElement(
     "div",
@@ -17,10 +17,29 @@ const myJsxElement = (
     </div>
 )
 
-console.log(myReactElement);
-console.log(myReactElement.props.name);
+const Age = (props) => {
+    return (
+        <p>I am {props.age} years old</p>
+    );
+}
+
+const MyFirstFN = (props) => {
+    console.log("my props", props.children);
+    return (
+        <div>
+            <p>Hello React Creat Element from Name: {props.name}</p>
+            {props.children}
+        </div>
+    );
+};
+
+// console.log(myJsxElement);
+// console.log(MyFirstFN());
 
 ReactDOM.createRoot(document.getElementById
     ("myDivRoot")).render(
-        myJsxElement
+        <>
+            <MyFirstFN name={"Hasu Apa"}></MyFirstFN>
+            <Age age={"25"}></Age>
+        </>
     );
