4548ac45c14dd4f7c0f1e9c4e820d9ffb566a0cb
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-06-23 04:29:34 +0600

    how react work

--- scratch/react.js
+++ scratch/react.js
@@ -36,10 +36,34 @@ const MyFirstFN = (props) => {
 // console.log(myJsxElement);
 // console.log(MyFirstFN());
 
+
+const Counter = () => {
+
+    const [myCounter, setMyCounter] = React.useState(0);
+
+    function handleAdd(){
+
+        setMyCounter(myCounter + 1);
+        // const prevCount = document.getElementById("counter").innerText;
+
+        // const parseNumber = parseInt(prevCount);
+        // document.getElementById("counter").innerText = parseNumber + 1;
+
+    }
+
+    return(
+        <div>
+            <button onClick={handleAdd}>Counter Clicked</button>
+            <p>Counter: <span id="counter">{myCounter}</span></p>
+        </div>
+    );
+};
+
 ReactDOM.createRoot(document.getElementById
     ("myDivRoot")).render(
         <>
-            <MyFirstFN name={"Hasu Apa"}></MyFirstFN>
-            <Age age={"25"}></Age>
+            {/* <MyFirstFN name={"Hasu Apa"}></MyFirstFN>
+            <Age age={"25"}></Age> */}
+            <Counter></Counter>
         </>
     );
