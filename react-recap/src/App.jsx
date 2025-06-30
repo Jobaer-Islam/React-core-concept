072329ccd019629f74ae6a844f8e6bc9f402a4d0
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-06-30 17:18:59 +0600

    controlled & uncontrolled component

--- react-recap/src/App.jsx
+++ react-recap/src/App.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const App = () => {
 
   const [counter, setCounter] = useState(0);
 
-  const handleLogin = (event) =>{
-    event.preventDefault();
-    console.log(event.target.name.value);
+  // const handleLogin = (event) =>{
+  //   event.preventDefault();
+  //   console.log(event.target.name.value);
+  // }
+
+  // const buttonRef = useRef(null);
+
+  // const handleClick = () =>{
+  //   alert("I got clicked!")
+  // }
+
+  // const buttonRef = useRef(null)
+  // const handleClick = () => {
+  //   console.log(buttonRef.current.value);
+  // }
+
+  const [name, setName] = useState('')
+  const handleLogin = (event) => {
+    event.preventDefault()
   }
+  console.log(name);
 
   return (
     <div>
@@ -16,10 +33,20 @@ const App = () => {
           className='input input-primary'
           type="text"
           name='name'
+          value={name}
+          onChange={(event)=> setName(event.target.value)}
           placeholder='Your Name'
           />
           <button type='submit' className='btn btn-accent'>Submit</button>
       </form>
+
+      {/* <input
+        className='input input-primary'
+        type="text"
+        ref={buttonRef}
+        placeholder='Your Name'
+      />
+      <button onClick={handleClick} className='btn btn-active'>Click</button> */}
 
     </div>
 
